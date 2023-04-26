517248e19ccde85ba3bee1197bc4605a0bda5cd8
Carlos Pineda <cara_p@unicah.edu>
2023-04-26 12:17:39 -0600

    Leyendo y escribiendo archivos con Node.js

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -2,5 +2,19 @@
 
 const fs = require('fs');
 
-const hello = 'Hello world';
-console.log(hello);
+// const hello = 'Hello world';
+// console.log(hello);
+
+//////////////////////////////////////////////////////////////////////////////
+// Leyendo y escribiendo archivos con Node.js
+
+// Esta funcion nos ayuda a leer archivos de sicrona, el primer parametro es la ubicacion del archivo y el segundo
+// es la codificación de caracteres
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+console.log(textIn);
+
+const textOut = `This is what we know about the avocado: ${textIn}. \nCreated on ${Date.now()}`;
+
+// Esta función nos ayuda a crear un archivo con texto, el primer parametro es el archivo que sera la salida
+fs.writeFileSync('./txt/output.txt', textOut);
+console.log('File written!');
