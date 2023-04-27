0ea97aa58dc2e972e0e6617e989a0c7870c43d2a
Carlos Pineda <cara_p@unicah.edu>
2023-04-27 14:58:27 -0600

    Leyendo y escribiendo archivos de forma asíncrona

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -8,6 +8,7 @@ const fs = require('fs');
 //////////////////////////////////////////////////////////////////////////////
 // Leyendo y escribiendo archivos con Node.js
 
+// Blocking, synchronous way
 // Esta funcion nos ayuda a leer archivos de sicrona, el primer parametro es la ubicacion del archivo y el segundo
 // es la codificación de caracteres
 const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
@@ -31,3 +32,24 @@ fs.readFile('./txt/input.txt', 'utf-8', (err, data) => {
     console.log(data);
 });
 console.log('Reading file...');
+
+/////////////////////////////////////////////////////////////////////////////
+// Leyendo y escribiendo en archivos de forma asíncrona
+
+// Forma asincrona
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+    if(err) return console.log('ERROR');
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+        console.log(data2);
+        fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+            console.log(data3);
+
+            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+                console.log('Your file has been written');
+            })
+            
+        });
+    });
+});
+
+console.log('Will read file!');
