0d8f4de00b854cb41fac774fe984c680c759bda2
Carlos Pineda <cara_p@unicah.edu>
2023-04-27 14:32:41 -0600

    Blocking y no blocking codigo Naturaleza asincrona

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -18,3 +18,16 @@ const textOut = `This is what we know about the avocado: ${textIn}. \nCreated on
 // Esta función nos ayuda a crear un archivo con texto, el primer parametro es el archivo que sera la salida
 fs.writeFileSync('./txt/output.txt', textOut);
 console.log('File written!');
+
+//////////////////////////////////////////////////////////////////////////////
+// Blocking y no blocking codigo: Naturalez asincrona de Node.js
+
+/*
+El codigo sincrono se puede considerear bloqueador, hace que cada linea espera por el resultado de la otra
+*/
+
+// Ejemplo de código asincrono
+fs.readFile('./txt/input.txt', 'utf-8', (err, data) => {
+    console.log(data);
+});
+console.log('Reading file...');
