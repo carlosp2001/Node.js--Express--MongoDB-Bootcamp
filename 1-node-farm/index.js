771ebcdfa9b1b8a8de96e8ff04e510c9adb18847
Carlos Pineda <cara_p@unicah.edu>
2023-04-28 14:20:44 -0600

    Contruyendo una API sencilla

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -43,16 +43,20 @@ console.log('Reading file...');
 
 // Forma asincrona
 fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-    if(err) return console.log('ERROR');
+    if (err) return console.log('ERROR');
     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
         console.log(data2);
         fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
             console.log(data3);
 
-            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-                console.log('Your file has been written');
-            })
-            
+            fs.writeFile(
+                './txt/final.txt',
+                `${data2}\n${data3}`,
+                'utf-8',
+                (err) => {
+                    console.log('Your file has been written');
+                }
+            );
         });
     });
 });
@@ -65,29 +69,45 @@ console.log('Will read file!');
 //////////////////////////////////////////////////////////////////////////////
 // Creando un servidor web simple
 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
     console.log(req.url);
 
     const pathName = req.url;
-    if(pathName === '/' || pathName === '/overview') {
+    if (pathName === '/' || pathName === '/overview') {
         res.end('This is the overview');
-    } else if(pathName === '/product') {
+    } else if (pathName === '/product') {
         res.end('This is the product');
+    } else if (pathName === '/api') {
+        // fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) => {
+        //     const productData = JSON.parse(data);
+        //     console.log(productData);
+        //     res.writeHead(200, {
+        //         'Content-type': 'application/json',
+        //     });
+        //     res.end(data);
+        // });
+        
+        res.writeHead(200, {
+            'Content-type': 'application/json',
+        });
+        res.end(data);
     } else {
         // De esta forma enviamos encabezados HTTP
         // Un encabezado HTTP es basicamente una información sobre la respuesta que estamos enviando
         res.writeHead(404, {
-            'Content-type' : 'text/html',
-            'my-own-header' : 'hello-world'
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world',
         });
-        res.end('Page not found!')
+        res.end('Page not found!');
     }
 });
 
 server.listen(8000, '127.0.0.1', () => {
     console.log('Listening to requests on port 8000');
-})
+});
 
 //////////////////////////////////////////////////////////////////////////////
 // Routing
-
