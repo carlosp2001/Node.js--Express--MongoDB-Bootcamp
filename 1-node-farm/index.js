6d4bfe25d28d832183f7c529e26bf29fbcb4b907
Carlos Pineda <cara_p@unicah.edu>
2023-04-30 15:11:28 -0600

    HTML Templating: Llenando los formatos

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -69,6 +69,34 @@ console.log('Will read file!');
 //////////////////////////////////////////////////////////////////////////////
 // Creando un servidor web simple
 
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCT_NAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+
+    if (!product.organic)
+        output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    return output;
+};
+
+const tempOverview = fs.readFileSync(
+    `${__dirname}/templates/template-overview.html`,
+    'utf-8'
+);
+const tempCard = fs.readFileSync(
+    `${__dirname}/templates/template-card.html`,
+    'utf-8'
+);
+const tempProduct = fs.readFileSync(
+    `${__dirname}/templates/template-product.html`,
+    'utf-8'
+);
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
@@ -76,10 +104,24 @@ const server = http.createServer((req, res) => {
     console.log(req.url);
 
     const pathName = req.url;
+
+    // Overview page
     if (pathName === '/' || pathName === '/overview') {
-        res.end('This is the overview');
+        res.writeHead(200, {
+            'Content-type': 'text/html',
+        });
+
+        const cardsHtml = dataObj.map((el) => replaceTemplate(tempCard, el)).join('');
+        console.log(cardsHtml);
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+
+        res.end(output);
+
+        // Product page
     } else if (pathName === '/product') {
         res.end('This is the product');
+
+        // API
     } else if (pathName === '/api') {
         // fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) => {
         //     const productData = JSON.parse(data);
@@ -94,6 +136,8 @@ const server = http.createServer((req, res) => {
             'Content-type': 'application/json',
         });
         res.end(data);
+
+        // Not found
     } else {
         // De esta forma enviamos encabezados HTTP
         // Un encabezado HTTP es basicamente una información sobre la respuesta que estamos enviando
@@ -115,3 +159,5 @@ server.listen(8000, '127.0.0.1', () => {
 //////////////////////////////////////////////////////////////////////////////
 // HTML Templating: Construyendo los formatos
 
+//////////////////////////////////////////////////////////////////////////////
+// HTML Templating: Llenando los formatos
