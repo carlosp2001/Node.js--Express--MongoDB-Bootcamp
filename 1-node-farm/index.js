163fbab320c9df9eef4d47cdaf639f35e46e282e
Carlos Pineda <cara_p@unicah.edu>
2023-05-01 15:53:22 -0600

    Introducción a NPM y el archivo package.json

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -3,6 +3,7 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const replaceTemplate = require('./modules/replaceTemplate');
 
 //////////////////////////////////////////////////////////////////////////////
 // ARCHIVOS
@@ -68,21 +69,6 @@ console.log('Will read file!');
 
 //////////////////////////////////////////////////////////////////////////////
 // Creando un servidor web simple
-
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCT_NAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-
-    if (!product.organic)
-        output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    return output;
-};
 
 const tempOverview = fs.readFileSync(
     `${__dirname}/templates/template-overview.html`,
@@ -173,3 +159,5 @@ server.listen(8000, '127.0.0.1', () => {
 //////////////////////////////////////////////////////////////////////////////
 // Obteniendo variables desde la url
 
+//////////////////////////////////////////////////////////////////////////////
+// Usando modulos 2: Nuestros propios módulos
