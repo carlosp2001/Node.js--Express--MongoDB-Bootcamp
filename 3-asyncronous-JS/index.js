57c01e7a119fcc24dd8554b0d4a95f3bfb4e331d
Carlos Pineda <cara_p@unicah.edu>
2023-05-07 11:30:34 -0600

    Creando promesas

--- 3-asyncronous-JS/index.js
+++ 3-asyncronous-JS/index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const {resolve} = require('path');
 const superagent = require('superagent');
 
 // Usando callback hell
@@ -37,3 +38,40 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
             console.log(err.message);
         });
 });
+
+const readFilePro = (file) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(file, (err, data) => {
+            if (err) reject('I could not find that file');
+            resolve(data);
+        });
+    });
+};
+
+const writeFilePro = (file, data) => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(file, data, (err) => {
+            if (err) reject('Could not write the file');
+            resolve('success');
+        });
+    });
+};
+
+readFilePro(`${__dirname}/dog.txt`)
+    .then((data) => {
+        console.log(`Breed: ${data}`);
+
+        return superagent.get(
+            `https://dog.ceo/api/breed/${data}/images/random`
+        );
+    })
+    .then((res) => {
+        console.log(res.body.message);
+       
+       return writeFilePro('dog-img.txt', res.body.message)
+    }).then((res) => {
+        console.log(res);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
