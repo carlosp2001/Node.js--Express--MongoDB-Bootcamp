d62b2fd978616d3cf4726f607948b5388e0190be
Carlos Pineda <cara_p@unicah.edu>
2023-05-07 10:43:50 -0600

    Usando promesas

--- 3-asyncronous-JS/index.js
+++ 3-asyncronous-JS/index.js
@@ -1,20 +1,39 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
+// Usando callback hell
+
+// fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+//     console.log(`Breed: ${data}`);
+
+//     superagent
+//         .get(`https://dog.ceo/api/breed/${data}/images/random`)
+//         .end((err, res) => {
+//             if(err) return console.log(err.message);
+//             console.log(res.body.message);
+
+//             fs.writeFile('dog-img.txt', res.body.message, err => {
+//                 if(err) return console.log(err.message);
+//                 console.log('Random dog image saved to file');
+//             })
+//         });
+// });
+
+// Promesas
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
     console.log(`Breed: ${data}`);
 
     superagent
         .get(`https://dog.ceo/api/breed/${data}/images/random`)
-        .end((err, res) => {
-            if(err) return console.log(err.message);
+        .then((res) => {
             console.log(res.body.message);
 
-            fs.writeFile('dog-img.txt', res.body.message, err => {
-                if(err) return console.log(err.message);
+            fs.writeFile('dog-img.txt', res.body.message, (err) => {
+                if (err) return console.log(err.message);
                 console.log('Random dog image saved to file');
-            })
+            });
+        })
+        .catch((err) => {
+            console.log(err.message);
         });
-
-
 });
