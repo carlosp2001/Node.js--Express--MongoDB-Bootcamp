aa16d5ec4a8b760647c22f01e98499c3c8519ae8
Carlos Pineda <cara_p@unicah.edu>
2023-05-09 11:49:58 -0600

    Manejando la solicitudes DELETE

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -82,10 +82,24 @@ app.patch('/api/v1/tours/:id', (req, res) => {
     }
     
     res.status(200).json({
-        status: 'Success',
+        status: 'success',
         data: {
             tour: '<Updated tour here...>'
         }
+    })
+});
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+    if(req.params.id * 1 >tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        })
+    }
+    
+    res.status(204).json({
+        status: 'success',
+        data: null
     })
 });
 
