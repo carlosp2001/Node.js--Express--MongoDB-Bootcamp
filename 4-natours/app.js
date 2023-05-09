df56e3ac5790e8d0d7c306ec2435fa3bba167f1e
Carlos Pineda <cara_p@unicah.edu>
2023-05-09 11:47:02 -0600

    Manejando la solicitudes PATCH

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -73,8 +73,23 @@ app.post('/api/v1/tours', (req, res) => {
     );
 });
 
-const port = 3000;
+app.patch('/api/v1/tours/:id', (req, res) => {
+    if(req.params.id * 1 >tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        })
+    }
+    
+    res.status(200).json({
+        status: 'Success',
+        data: {
+            tour: '<Updated tour here...>'
+        }
+    })
+});
 
+const port = 3000;
 app.listen(port, () => {
     console.log(`App running on port ${port}...`);
 });
