5083af66537d21fe997143f6e2203d1b22d7a8f8
Carlos Pineda <cara_p@unicah.edu>
2023-05-09 11:58:53 -0600

    Refactorizando nuestras rutas

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -21,37 +21,36 @@ const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
     res.status(200).json({
         status: 'success',
         results: tours.length,
         data: {tours},
     });
-});
+};
 
-app.get('/api/v1/tours/:id', (req, res) => {
+const getTour = (req, res) => {
     // Podemos crear parametro opcional agregando ?
     console.log(req.params);
     const id = req.params.id * 1;
     const tour = tours.find((el) => el.id === id);
 
     // if (id>tours.length)
-    if(!tour) {
+    if (!tour) {
         return res.status(404).json({
             status: 'fail',
-            message: 'Invalid ID'
-        })
+            message: 'Invalid ID',
+        });
     }
 
-    
     console.log(tour);
     res.status(200).json({
         status: 'success',
         data: {tour},
     });
-});
+};
 
-app.post('/api/v1/tours', (req, res) => {
+const createTour = (req, res) => {
     // Req contiene toda la información sobre la solicitud que se realizó, los datos que se envian estan
     // contenidos acá
     // console.log(req.body);
@@ -71,37 +70,50 @@ app.post('/api/v1/tours', (req, res) => {
             }); // El codigo 201 significado creado
         }
     );
-});
+};
 
-app.patch('/api/v1/tours/:id', (req, res) => {
-    if(req.params.id * 1 >tours.length) {
+const updateTour = (req, res) => {
+    if (req.params.id * 1 > tours.length) {
         return res.status(404).json({
             status: 'fail',
-            message: 'Invalid ID'
-        })
+            message: 'Invalid ID',
+        });
     }
-    
+
     res.status(200).json({
         status: 'success',
         data: {
-            tour: '<Updated tour here...>'
-        }
-    })
-});
+            tour: '<Updated tour here...>',
+        },
+    });
+};
 
-app.delete('/api/v1/tours/:id', (req, res) => {
-    if(req.params.id * 1 >tours.length) {
+const deleteTour = (req, res) => {
+    if (req.params.id * 1 > tours.length) {
         return res.status(404).json({
             status: 'fail',
-            message: 'Invalid ID'
-        })
+            message: 'Invalid ID',
+        });
     }
-    
+
     res.status(204).json({
         status: 'success',
-        data: null
-    })
-});
+        data: null,
+    });
+};
+
+// app.get('/api/v1/tours', getAllTours);
+// app.post('/api/v1/tours', createTour);
+
+// app.get('/api/v1/tours/:id', getTour);
+// app.patch('/api/v1/tours/:id', updateTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
+
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+app.route('/api/v1/tours/:id')
+    .get(getTour)
+    .patch(updateTour)
+    .delete(deleteTour);
 
 const port = 3000;
 app.listen(port, () => {
