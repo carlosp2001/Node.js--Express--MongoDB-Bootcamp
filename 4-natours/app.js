44649ff90530d34387f9dcc7aa33e3aff01312b3
Carlos Pineda <cara_p@unicah.edu>
2023-05-09 10:41:01 -0600

    Respondiendo a los parametros URL

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -29,6 +29,28 @@ app.get('/api/v1/tours', (req, res) => {
     });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+    // Podemos crear parametro opcional agregando ?
+    console.log(req.params);
+    const id = req.params.id * 1;
+    const tour = tours.find((el) => el.id === id);
+
+    // if (id>tours.length)
+    if(!tour) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        })
+    }
+
+    
+    console.log(tour);
+    res.status(200).json({
+        status: 'success',
+        data: {tour},
+    });
+});
+
 app.post('/api/v1/tours', (req, res) => {
     // Req contiene toda la información sobre la solicitud que se realizó, los datos que se envian estan
     // contenidos acá
