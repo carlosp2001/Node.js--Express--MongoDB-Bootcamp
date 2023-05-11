d98b6908863bd07fc59891f99f8c766876ee4e0c
Carlos Pineda <cara_p@unicah.edu>
2023-05-11 11:29:40 -0600

    Creando y montando multiples rutas

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -165,19 +165,23 @@ const deleteUser = (req, res, next) => {
 
 // 3) ROUTES
 
-app.route('/api/v1/tours').get(getAllTours).post(createTour);
+const tourRouter = express.Router();
+const userRouter = express.Router();
 
-app.route('/api/v1/tours/:id')
-    .get(getTour)
-    .patch(updateTour)
-    .delete(deleteTour);
+tourRouter.route('/').get(getAllTours).post(createTour);
 
-app.route('/api/v1/users').get(getAllUsers).post(createUser);
+tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
-app.route('/api/v1/users/:id')
+userRouter.route('/').get(getAllUsers).post(createUser);
+
+userRouter
+    .route('/:id')
     .get(getUser)
     .patch(updateUser)
     .delete(deleteUser);
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 // 4) START SERVER
 const port = 3000;
