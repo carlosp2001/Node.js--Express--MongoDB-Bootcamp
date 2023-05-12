b66c010098c327f22a41b8b6e8e5ba609a525fe6
Carlos Pineda <cara_p@unicah.edu>
2023-05-11 20:45:29 -0600

    Encadenando multiples funciones middleware

--- 4-natours/routes/tourRoutes.js
+++ 4-natours/routes/tourRoutes.js
@@ -1,11 +1,23 @@
 const express = require('express');
-const tourController = require('./../controllers/tourController')
+const tourController = require('./../controllers/tourController');
 const router = express.Router();
 
-router.param('id', tourController.checkID)
+router.param('id', tourController.checkID);
 
-router.route('/').get(tourController.getAllTours).post(tourController.createTour);
+// Create a checkBody middleware function
+// Check if the body contains the name and price property
+// If not, send back 400 (bad request)
+// Add it to the post handler stack
 
-router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour);
+router
+    .route('/')
+    .get(tourController.getAllTours)
+    .post(tourController.checkBody, tourController.createTour);
+
+router
+    .route('/:id')
+    .get(tourController.getTour)
+    .patch(tourController.updateTour)
+    .delete(tourController.deleteTour);
 
 module.exports = router;
