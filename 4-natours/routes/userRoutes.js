9c5ad318ce2556533c7838a39d320fe8bf282cc4
Carlos Pineda <cara_p@unicah.edu>
2023-05-11 20:16:13 -0600

    Una mejor estructura de los archivos

---
+++ 4-natours/routes/userRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const userController = require('./../controllers/userController');
+
+// app.get('/api/v1/tours', getAllTours);
+// app.post('/api/v1/tours', createTour);
+
+// app.get('/api/v1/tours/:id', getTour);
+// app.patch('/api/v1/tours/:id', updateTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
+const router = express.Router();
+
+router.route('/').get(userController.getAllUsers).post(userController.createUser);
+router.route('/:id').get(userController.getUser).patch(userController.updateUser).delete(userController.deleteUser);
+
+module.exports = router;
