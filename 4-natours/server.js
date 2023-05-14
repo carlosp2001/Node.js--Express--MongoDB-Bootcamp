75f33d21f59bc735c7b2bdc79d1a25a07ac04573
Carlos Pineda <cara_p@unicah.edu>
2023-05-13 20:47:09 -0600

    Conectando nuestra base de datos con mongoose

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,10 +1,27 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
 
 dotenv.config({
     path: './config.env',
 });
-const app = require('./app');
 
+const DB = process.env.DATABASE.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD
+);
+
+mongoose
+    .connect(DB, {
+        // .connect(process.env.DATABASE_LOCAL, { // Base de datos local
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+    })
+    .then(() => {
+        // console.log(con.connections);
+        console.log('DB Connection Successful!');
+    });
 // console.log(process.env);
 
 const port = process.env.PORT || 3000;
