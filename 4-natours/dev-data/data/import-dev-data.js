38c4d46a024226073f85d61d2d2d9f62b5f0f73c
Carlos Pineda <cara_p@unicah.edu>
2023-05-18 10:25:08 -0600

    Script para cargar datos json a la base de datos

---
+++ 4-natours/dev-data/data/import-dev-data.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('../../models/tourModel');
+
+dotenv.config({
+    path: './config.env',
+});
+
+const DB = process.env.DATABASE.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD
+);
+
+mongoose
+    .connect(DB, {
+        // .connect(process.env.DATABASE_LOCAL, { // Base de datos local
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+    })
+    .then(() => {
+        // console.log(con.connections);
+        console.log('DB Connection Successful!');
+    });
+// console.log(process.env);
+
+// Read file
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+// Import data into db
+const importData = async () => {
+    try {
+        await Tour.create(tours);
+        console.log('Data successfully loaded!');
+        process.exit();
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+// Delete all data from db
+const deleteData = async () => {
+    try {
+        await Tour.deleteMany();
+        console.log('Data successfully deleted!');
+        process.exit();
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+if (process.argv[2] === '--import') {
+    importData();
+} else if (process.argv[2] === '--delete') {
+    deleteData();
+}
+console.log(process.argv);
