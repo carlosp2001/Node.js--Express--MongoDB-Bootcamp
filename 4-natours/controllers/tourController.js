bed56051dbf78cdfc6c8ea4be74504259112668f
Carlos Pineda <cara_p@unicah.edu>
2023-05-20 10:52:49 -0600

    Mejorando la API: Filtrar

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -30,7 +30,29 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
     try {
-        const tours = await Tour.find();
+        // BUILD QUERY
+        const queryObj = { ...req.query };
+        const excludedFields = ['page', 'sort', 'limit', 'fields'];
+        excludedFields.forEach((el) => delete queryObj[el]);
+
+        const query = Tour.find(queryObj);
+        console.log(req.query, queryObj);
+
+        // const query = await Tour.find({
+        //     duration: 5,
+        //     difficulty: 'easy',
+        // });
+
+        // const query = await Tour.find()
+        //     .where('duration')
+        //     .equals(5)
+        //     .where('difficulty')
+        //     .equals('easy');
+
+        // EXECUTE QUERY
+        const tours = await query;
+
+        // SEND RESPONSE
         console.log(req.requestTime);
         res.status(200).json({
             status: 'success',
@@ -43,6 +65,7 @@ exports.getAllTours = async (req, res) => {
             status: 'fail',
             message: err,
         });
+        console.log(err);
     }
 };
 
