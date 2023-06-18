7d4b200a485d113881a98ecbcbddb0d6874c8206
Carlos Pineda <cara_p@unicah.edu>
2023-06-18 15:39:31 -0600

    Capturando excepciones no detectadas

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+    console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+    console.log(err.name, err.message);
+
+    process.exit(1);
+});
+
 dotenv.config({
     path: './config.env',
 });
+
 const app = require('./app');
 
 const DB = process.env.DATABASE.replace(
@@ -25,8 +33,19 @@ mongoose
 // console.log(process.env);
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`App running on port ${port}...`);
 });
 
 // TEST
+
+process.on('unhandledRejection', (err) => {
+    // Codigo 0 significa representa exito, Codigo 1 representa una excepcion no detectada
+    console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+    console.log(err);
+    server.close(() => {
+        process.exit(1);
+    });
+});
+
+// console.log(x);
