551126b2be3bbfc0f4da080dbe62cc56a90844a0
Carlos Pineda <cara_p@unicah.edu>
2023-06-18 13:28:25 -0600

    Manejando ids invalidos en la base de datos

--- 4-natours/controllers/errorController.js
+++ 4-natours/controllers/errorController.js
@@ -1,3 +1,10 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+    const message = `Invalid ${err.path}: ${err.value}.`;
+    return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
         status: err.status,
@@ -18,7 +25,7 @@ const sendErrorProd = (err, res) => {
         // Programming or other unknown error: don't leak details to the client
     } else {
         // 1) Log error
-        console.error('ERROR 💥', err);
+        // console.error('ERROR 💥', err);
 
         // 2) Send generic message
         res.status(500).json({
@@ -29,7 +36,6 @@ const sendErrorProd = (err, res) => {
 };
 
 module.exports = (err, req, res, next) => {
-    console.log(err.stack);
 
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
@@ -37,6 +43,9 @@ module.exports = (err, req, res, next) => {
     if (process.env.NODE_ENV === 'development') {
         sendErrorDev(err, res);
     } else if (process.env.NODE_ENV === 'production') {
-        sendErrorProd(err, res);
+        let error = { ...err };
+        console.log(error);
+        if (err.name === 'CastError') error = handleCastErrorDB(error);
+        sendErrorProd(error, res);
     }
 };
