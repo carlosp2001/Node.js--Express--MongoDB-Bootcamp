ece1cbddb295cdd3abe653059f5ada8ffa7f88fb
Carlos Pineda <cara_p@unicah.edu>
2023-06-26 21:06:55 -0600

    Logging in Users

--- 4-natours/controllers/userController.js
+++ 4-natours/controllers/userController.js
@@ -1,9 +1,18 @@
-exports.getAllUsers = (req, res, next) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined',
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+    const tours = await User.find();
+    // query.sort().select().skip().limit()
+
+    // SEND RESPONSE
+    res.status(200).json({
+        status: 'success',
+        // requestedAt: req.requestTime,
+        results: tours.length,
+        data: { tours },
     });
-};
+});
 
 exports.getUser = (req, res, next) => {
     res.status(500).json({
