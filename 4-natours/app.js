40f4b405cb47955d571ffdf9fc92033464fb8fb0
Carlos Pineda <cara_p@unicah.edu>
2023-09-25 19:57:19 -0600

    Configurando Pug en Express para el manejo de views

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,17 +1,18 @@
 // En el archivo app.js mantenemos nuestra configuración de express
-const express = require('express');
-const morgan = require('morgan');
-const rateLimit = require('express-rate-limit');
-const helmet = require('helmet');
-const mongoSanitize = require('express-mongo-sanitize');
-const xss = require('xss-clean');
-const hpp = require('hpp');
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
 
-const AppError = require('./utils/appError');
-const globalErrorHandler = require('./controllers/errorController');
-const tourRouter = require('./routes/tourRoutes');
-const userRouter = require('./routes/userRoutes');
-const reviewRouter = require('./routes/reviewRoutes');
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
+const tourRouter = require("./routes/tourRoutes");
+const userRouter = require("./routes/userRoutes");
+const reviewRouter = require("./routes/reviewRoutes");
 
 const app = express();
 
@@ -25,54 +26,62 @@ const app = express();
 //   res.send('You can post to this endpoint...');
 // });
 
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
 // 1) GLOBAL MIDDLEWARES
+
+// Serving static files
+// app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, "public")));
+
 // Set security HTTP Headers
 app.use(helmet());
 console.log(process.env.NODE_ENV);
 
 // Development logging
-if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 // Liimit requests from same API
 const limiter = rateLimit({
-    max: 100,
-    windowMs: 60 * 60 * 1000,
-    message: 'Too many request from this IP, please try again in an hour!',
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many request from this IP, please try again in an hour!"
 });
 
-app.use('/api', limiter);
+app.use("/api", limiter);
 
 // Body parser, reading data from the body into req.body
 app.use(
-    express.json({
-        limit: '10kb',
-    })
-); // Este es el middleware, es una función que puede modificar los datos de la solicitud entrante, se llama middleware porque se encuentra en medio de la solicitud y la respuesta
+  express.json({
+    limit: "10kb"
+  })
+); // Este es el middleware, es una función que puede modificar los datos de la
+// solicitud entrante, se llama middleware porque se encuentra en medio de la
+// solicitud y la respuesta
 
 // Data sanitization against NoSQL query injection
-app.use(mongoSanitize()); // Elimina todo tipo de signo que este relacionado con operadores de mongoose queries
+app.use(mongoSanitize()); // Elimina todo tipo de signo que este relacionado con
+// operadores de mongoose queries
 
 // Data sanitization against XSS
 app.use(xss());
 
 // Prevent parameter pollution
 app.use(
-    hpp({
-        whitelist: [
-            'duration',
-            'ratingsQuantity',
-            'ratingsAverage',
-            'maxGroupSize',
-            'difficulty',
-            'price',
-        ],
-    })
+  hpp({
+    whitelist: [
+      "duration",
+      "ratingsQuantity",
+      "ratingsAverage",
+      "maxGroupSize",
+      "difficulty",
+      "price"
+    ]
+  })
 );
-
-// Serving static files
-app.use(express.static(`${__dirname}/public`));
 
 // app.use((req, res, next) => {
 //     console.log('Hello from the middleware');
@@ -81,28 +90,31 @@ app.use(express.static(`${__dirname}/public`));
 
 // Test middleware
 app.use((req, res, next) => {
-    req.requestTime = new Date().toISOString();
-    console.log(req.headers);
-    next();
+  req.requestTime = new Date().toISOString();
+  console.log(req.headers);
+  next();
 });
 
 // 3) ROUTES
+app.get('/', (req, res) => {
+  res.status(200).render('base');
+})
 
-app.use('/api/v1/tours', tourRouter);
-app.use('/api/v1/users', userRouter);
-app.use('/api/v1/reviews', reviewRouter);
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/reviews", reviewRouter);
 
-app.all('*', (req, res, next) => {
-    // res.status(404).json({
-    //     status: 'fail',
-    //     message: `Can't find ${req.originalUrl} on this server`,
-    // });
+app.all("*", (req, res, next) => {
+  // res.status(404).json({
+  //     status: 'fail',
+  //     message: `Can't find ${req.originalUrl} on this server`,
+  // });
 
-    // const err = new Error(`Can't find ${req.originalUrl} on this server!`);
-    // err.status = 'fail';
-    // err.statusCode = 404;
+  // const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
 
-    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
 app.use(globalErrorHandler);
