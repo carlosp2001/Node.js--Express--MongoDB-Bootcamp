c8b9ac7c34f5005c17fc3c5f1ae253efe7469752
Carlos Pineda <cara_p@unicah.edu>
2023-09-27 20:54:08 -0600

    Extendiendo nuestro template base con bloques

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,18 +1,18 @@
 // En el archivo app.js mantenemos nuestra configuración de express
-const path = require("path");
-const express = require("express");
-const morgan = require("morgan");
-const rateLimit = require("express-rate-limit");
-const helmet = require("helmet");
-const mongoSanitize = require("express-mongo-sanitize");
-const xss = require("xss-clean");
-const hpp = require("hpp");
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
-const AppError = require("./utils/appError");
-const globalErrorHandler = require("./controllers/errorController");
-const tourRouter = require("./routes/tourRoutes");
-const userRouter = require("./routes/userRoutes");
-const reviewRouter = require("./routes/reviewRoutes");
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 
@@ -26,37 +26,37 @@ const app = express();
 //   res.send('You can post to this endpoint...');
 // });
 
-app.set("view engine", "pug");
-app.set("views", path.join(__dirname, "views"));
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
 // 1) GLOBAL MIDDLEWARES
 
 // Serving static files
 // app.use(express.static(`${__dirname}/public`));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Set security HTTP Headers
 app.use(helmet());
 console.log(process.env.NODE_ENV);
 
 // Development logging
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 
 // Liimit requests from same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
-  message: "Too many request from this IP, please try again in an hour!"
+  message: 'Too many request from this IP, please try again in an hour!'
 });
 
-app.use("/api", limiter);
+app.use('/api', limiter);
 
 // Body parser, reading data from the body into req.body
 app.use(
   express.json({
-    limit: "10kb"
+    limit: '10kb'
   })
 ); // Este es el middleware, es una función que puede modificar los datos de la
 // solicitud entrante, se llama middleware porque se encuentra en medio de la
@@ -73,12 +73,12 @@ app.use(xss());
 app.use(
   hpp({
     whitelist: [
-      "duration",
-      "ratingsQuantity",
-      "ratingsAverage",
-      "maxGroupSize",
-      "difficulty",
-      "price"
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price'
     ]
   })
 );
@@ -96,18 +96,30 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
-app.get("/", (req, res) => {
-  res.status(200).render("base", {
-    tour: "The Forest Hiker",
-    user: "Jonas"
+app.get('/', (req, res) => {
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Jonas'
   });
 });
 
-app.use("/api/v1/tours", tourRouter);
-app.use("/api/v1/users", userRouter);
-app.use("/api/v1/reviews", reviewRouter);
+app.get('/overview', (req, res) => {
+  res.status(200).render('overview', {
+    title: 'All tours'
+  });
+});
 
-app.all("*", (req, res, next) => {
+app.get('/tour', (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker Tour'
+  });
+});
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
+
+app.all('*', (req, res, next) => {
   // res.status(404).json({
   //     status: 'fail',
   //     message: `Can't find ${req.originalUrl} on this server`,
